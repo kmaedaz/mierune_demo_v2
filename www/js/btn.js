1be817aeb982224f5654ff3ca70c90bef0834646
kmaedaz <maeda.ko1ro@gmail.com>
2017-01-16 13:59:18 +0900

    .

--- www/js/btn.js
+++ www/js/btn.js
@@ -2,7 +2,12 @@
 
 function setBtnAnim() {
 	// ボタンタップ開始時、終了時にアニメーションクラスを与える
-	$('.btn_tap_anim').bind('touchstart mousedown',					function() { $(this).addClass('btn_tap_anim_on'); });
-	$('.btn_tap_anim').bind('touchmove touchend mouseup mouseout',	function() { $(this).removeClass('btn_tap_anim_on'); });
+	$('.btn_tap_anim').bind('touchstart mousedown',	function() { $(this).addClass('btn_tap_anim_on');
+            	console.log("setBtnAnim");
+    });
+	$('.btn_tap_anim').bind('touchmove touchend mouseup mouseout',	function() {
+        $(this).removeClass('btn_tap_anim_on');
+
+        });
 }
 
