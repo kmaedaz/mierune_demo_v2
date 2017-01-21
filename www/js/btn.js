4d409406794c3c28d2e943098cc716657a7f577c
kmaedaz <maeda.ko1ro@gmail.com>
2017-01-21 14:49:08 +0900

    .

--- www/js/btn.js
+++ www/js/btn.js
@@ -2,12 +2,28 @@
 
 function setBtnAnim() {
 	// ボタンタップ開始時、終了時にアニメーションクラスを与える
-	$('.btn_tap_anim').bind('touchstart mousedown',	function() { $(this).addClass('btn_tap_anim_on');
+	$('.btn_tap_anim' ).bind('touchstart mousedown',	function() { $(this).addClass('btn_tap_anim_on');
             	console.log("setBtnAnim");
     });
-	$('.btn_tap_anim').bind('touchmove touchend mouseup mouseout',	function() {
+
+    $('.btn_tap_anim').bind('touchmove touchend mouseup mouseout',	function() {
         $(this).removeClass('btn_tap_anim_on');
+                console.log("btn_tap_anim_on");
 
         });
+
+    $('a' ).bind('touchstart mousedown',	function() { $(this).addClass('btn_tap_anim_on');
+            	console.log("setBtnAnim");
+    });
+
+    $('a').bind('touchmove touchend mouseup mouseout',	function() {
+        $(this).removeClass('btn_tap_anim_on');
+                console.log("btn_tap_anim_on");
+
+        });
+
+
+
+
 }
 
