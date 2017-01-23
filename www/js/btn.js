40b783eb8827d70250c5f2b9e7b8fef54675640a
kmaedaz <maeda.ko1ro@gmail.com>
2017-01-23 17:17:01 +0900

    リリース1.0

--- www/js/btn.js
+++ www/js/btn.js
@@ -12,18 +12,18 @@ function setBtnAnim() {
 
         });
 
-    $('a' ).bind('touchstart mousedown',	function() { $(this).addClass('btn_tap_anim_on');
-            	console.log("setBtnAnim");
-    });
-
-    $('a').bind('touchmove touchend mouseup mouseout',	function() {
-        $(this).removeClass('btn_tap_anim_on');
-                console.log("btn_tap_anim_on");
-
-        });
-
-
-
 
 }
 
+
+
+
+function buttonAnim(v) {
+$(v).addClass('btn_tap_anim_on');
+   console.log("buttonAnim"+v);
+    $(v).css("transform: scale(0.95)");
+    setTimeout(	function (){
+    $(v).css("transform: scale(1.0)");
+    } , 3000);
+
+}
